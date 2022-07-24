66ce2b764278cbe06d466a4830f787684a7dc75f
sharmaprasid <51880083+sharmaprasid@users.noreply.github.com>
2022-07-24 20:21:10 +0545

    first commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8e9c041d53msh5ee132b19d900cep14f296jsn4e62b836445b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "c974fc72347d1f43729b757e3697dd56"; 
